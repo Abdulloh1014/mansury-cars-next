794b7ea4aa470e002a8c605ea9cf4904f128eb92
Abdulloh1014 <abdullohmansurbekov73@gmail.com>
2026-02-22 20:28:22 +0900

    fix: enumlar modify qilindi

--- libs/enums/property.enum.ts
+++ libs/enums/property.enum.ts
@@ -1,16 +1,38 @@
-export enum PropertyType {
-	APARTMENT = 'APARTMENT',
-	VILLA = 'VILLA',
-	HOUSE = 'HOUSE',
+export enum CarType {
+    SEDAN = 'SEDAN',
+    SUV = 'SUV',
+    HATCHBACK = 'HATCHBACK',
+    COUPE = 'COUPE',
+    ELECTRIC = 'ELECTRIC',
+    PICKUP = 'PICKUP',
 }
 
-export enum PropertyStatus {
+export enum CarColor {
+	BLACK = 'BLACK',
+	WHITE = 'WHITE',
+	GRAY = 'GRAY',
+	RED = 'RED',
+	BLUE = 'BLUE',
+	SILVER = 'SILVER',
+	GREEN = 'GREEN',
+	YELLOW = 'YELLOW',
+	BROWN = 'BROWN',
+}
+
+export enum CarFuelType {
+	GASOLINE = 'GASOLINE',
+	DIESEL = 'DIESEL',
+	ELECTRIC = 'ELECTRIC',
+	HYBRID = 'HYBRID',
+}
+
+export enum CarStatus {
 	ACTIVE = 'ACTIVE',
 	SOLD = 'SOLD',
 	DELETE = 'DELETE',
 }
 
-export enum PropertyLocation {
+export enum CarLocation {
 	SEOUL = 'SEOUL',
 	BUSAN = 'BUSAN',
 	INCHEON = 'INCHEON',
